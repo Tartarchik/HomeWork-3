785009aa68a7a67ccf2d4351376c41e24ee013dd
renat <renat1703@gmail.com>
2023-04-25 16:12:49 +0300

    Fix

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -7,18 +7,30 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import page.components.Attachments;
 
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        Configuration.browserSize = System.getProperty("browserSize","1920x1080");;
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion","100");
+        Configuration.baseUrl = System.getProperty("baseUrl","https://demoqa.com");
+        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.pageLoadTimeout = 30000;
 
-        Configuration.pageLoadTimeout = 100000;
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
